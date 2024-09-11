cc7ef920cc03b77466afae9b19d077a1d738ccd8
joelward2025 <104170342+joelward2025@users.noreply.github.com>
2024-09-11 18:33:15 -0400

    Initial Commit

---
+++ HiLoFlip/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HiLoFlip
+//
+//  Created by Joel Ward on 9/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HiLoFlip/HiLoFlipApp.swift
@@ -0,0 +1,17 @@
+//
+//  HiLoFlipApp.swift
+//  HiLoFlip
+//
+//  Created by Joel Ward on 9/11/24.
+//
+
+import SwiftUI
+
+@main
+struct HiLoFlipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
