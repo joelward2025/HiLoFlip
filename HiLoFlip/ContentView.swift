f40c0a46876a5a32f51595f4fe566e08142725e7
joelward2025 <104170342+joelward2025@users.noreply.github.com>
2024-09-16 23:48:38 -0400

    update

--- HiLoFlip/ContentView.swift
+++ HiLoFlip/ContentView.swift
@@ -7,10 +7,13 @@
 
 import SwiftUI
 
+var CardOrder = Array(1...100).shuffled()[0..<7]
+
 func colorForIndex(_ index: Int) -> Color {
     let hue = Double(index) / 100.0
     return Color(hue: hue, saturation: 0.8, brightness: 1)
 }
+
 
 func getSymbol(i : Int) -> String {
     if i == 0 {
@@ -22,14 +25,15 @@ func getSymbol(i : Int) -> String {
     else {
         return "doc.on.doc.fill"
     }
-    
 }
+
 
 var HI: some View {
     HStack{
         ZStack{
             Circle()
-                .stroke(.white).padding(10)
+                .stroke(.white)
+                .padding(10)
                 .frame(width: 70, height: 70)
             Text("HI")
                 .font(.title)
@@ -40,12 +44,14 @@ var HI: some View {
     }
 }
 
+
 var LO: some View {
     HStack{
         Spacer()
         ZStack{
             Circle()
-                .stroke(.white).padding(10)
+                .stroke(.white)
+                .padding(10)
                 .frame(width: 70, height: 70)
             Text("LO")
                 .font(.title)
@@ -55,9 +61,91 @@ var LO: some View {
     }
 }
 
+
+var HiLoToken: some View {
+    ZStack{
+        Circle()
+            .frame(width: 150, height: 150)
+            .foregroundStyle(.black)
+            .overlay(Circle().stroke(Color.white, lineWidth: 2).frame(width: 130, height: 130))
+        Text("LO")
+            .font(.largeTitle)
+            .fontWeight(.bold)
+            .foregroundStyle(.white)
+    }.padding()
+}
+
+
+struct CornerSymbols: View {
+    var i: Int
+    var body: some View {
+        VStack {
+            HStack{
+                Symbol(i: i)
+                Spacer()
+            }
+            Spacer()
+            HStack{
+                Spacer()
+                Symbol(i: i)
+            }
+        }
+    }
+}
+
+
+struct Symbol: View {
+    var i: Int
+    var body: some View {
+        ZStack{
+            Circle()
+                .foregroundStyle(.white)
+                .frame(width: 25, height: 25)
+            Image(systemName: getSymbol(i: i % 10))
+                .foregroundStyle(.black)
+        }
+        .padding(5)
+    }
+}
+
+
+struct CenterCircle: View {
+    var i: Int
+    var body: some View {
+        Circle()
+            .frame(width: 70, height: 70)
+        Text(String(i))
+            .font(.largeTitle)
+            .fontWeight(.bold)
+            .foregroundStyle(.white)    }
+}
+
+
+var ShuffleButton: some View {
+    Button(action: {
+        CardOrder = Array(1...100).shuffled()[0..<7]
+            }) {
+                Text("Shuffle")
+                    .font(.headline)
+                    .padding()
+                    .background(Color.blue)
+                    .foregroundColor(.white)
+                    .cornerRadius(8)
+            }
+}
+
+var HandView: some View {
+    ScrollView{
+        LazyVGrid(columns: [GridItem(.adaptive(minimum: 100))], content: {
+            ForEach(CardOrder, id: \.self) { item in
+                CardView(i: item)
+            }
+        }).padding()
+    }
+}
+
 struct CardView: View {
     var i: Int
-    @State var corner: String = ""
     @State var up: Bool = true
     var body: some View {
         ZStack{
@@ -72,61 +160,41 @@ struct CardView: View {
             else {
                 RoundedRectangle(cornerRadius: 15.0)
                     .fill(colorForIndex(i))
-                VStack(spacing: 0){
-                    if i % 10 < 3 {
-                        HStack {
-                            ZStack{
-                                Circle()
-                                    .foregroundStyle(.white)
-                                    .frame(width: 25, height: 25)
-                                
-                                Image(systemName: getSymbol(i: i % 10))
-                                    .foregroundStyle(.black)
-                            }
-                            .padding(.top, 5)
-                            .padding(.leading, 5)
-                            Spacer()
-                        }
-                    }
-                    Spacer()
-                    ZStack {
-                        Circle()
-                            .frame(width: 70, height: 70)
-                        Text(String(i))
-                            .font(.largeTitle)
-                            .fontWeight(.bold)
-                            .foregroundStyle(.white)
-                    }
-                    Spacer()
-                    if i % 10 < 3 {
-                        HStack {
-                            Spacer()
-                            ZStack{
-                                Circle()
-                                    .foregroundStyle(.white)
-                                    .frame(width: 25, height: 25)
-                                
-                                Image(systemName: getSymbol(i: i % 10))
-                                    .foregroundStyle(.black)
-                            }
-                            .padding(.bottom, 5)
-                            .padding(.trailing, 5)
-                        }
-                    }
+                CenterCircle(i: i)
+                if i % 10 < 3 {
+                    CornerSymbols(i: i)
                 }
             }
         }.frame(width: 100, height: 155)
-        .onTapGesture {
-                    up = !up
+            .onTapGesture {
+                up = !up
+            }
+    }
+}
+
+
+
+struct GameView: View {
+    var body: some View {
+        ZStack{
+            // hex: #007700
+            Color(red: 0, green: (119/255), blue: 0).ignoresSafeArea()
+            VStack {
+                HStack{
+                    Spacer()
+                    HiLoToken
+                    ShuffleButton
+                    Spacer()
                 }
+                HandView
+            }
+        }
     }
 }
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            CardView(i: 60)
-        }
+        GameView()
     }
 }
 
